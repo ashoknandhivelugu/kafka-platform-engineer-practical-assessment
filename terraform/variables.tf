b29123e29539cf34c79b42fcbd9b8c0d7285a4b0
ashoknandhivelugu <ashokreddy.nandhivelugu@gmail.com>
2026-09-21 22:19:38 -0400

    Define variables for Confluent Cloud and region
    Added variables for Confluent Cloud API key, secret, and GCP region.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,17 @@
+variable "confluent_cloud_api_key" {
+  type        = string
+  description = "Confluent Cloud API Key for the Terraform Provider"
+  sensitive   = true
+}
 
+variable "confluent_cloud_api_secret" {
+  type        = string
+  description = "Confluent Cloud API Secret for the Terraform Provider"
+  sensitive   = true
+}
+
+variable "region" {
+  type        = string
+  default     = "us-central1"
+  description = "GCP region where the cluster will be deployed"
+}
