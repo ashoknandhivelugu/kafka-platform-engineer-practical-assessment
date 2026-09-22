b113f88ff16f26900682991e27ad611ba85022c1
ashoknandhivelugu <ashokreddy.nandhivelugu@gmail.com>
2026-09-21 22:21:43 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,5 @@
+output "kafka_bootstrap_endpoint" {
+  value       = confluent_kafka_cluster.standard.bootstrap_endpoint
+  description = "The Kafka cluster bootstrap URL needed by producers/consumers to establish connection."
+}
 
